eafe0465e80bad306a1ddfc8124b7e4a2ec99844
talesaraujo <talesaraujodossantos@gmail.com>
2019-11-06 00:48:06 -0300

    Set up testing environment

---
+++ wordprocessor/tests/test_wordprocessor.py
@@ -0,0 +1,13 @@
+import unittest
+
+from unittest import TestCase
+
+"""
+    Sugestão de abordagem
+
+    Requisito funcional a ser tratado: 
+    RF001 - Realizar operações de gerenciamento: abrir, salvar, salvar como
+
+    Classe abordada: MainWindow
+    Métodos: file_save() e file_saveas()
+"""
